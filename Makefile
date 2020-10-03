e7633a2b8272823d7c32a71235bf10a64800c86f
Bapi Gupta <rajat310198@outlook.com>
2020-10-03 11:58:40 +0530

    Added benchmarking

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ all: lint test
 test:
 	go test
 
+bench:
+	go test -bench=.
+
 lint:
 	go fmt ./...
 
--- gologger_test.go
+++ gologger_test.go
@@ -81,3 +81,91 @@ func TestGoLogger_Debug2(t *testing.T) {
 
 	wg.Wait()
 }
+
+func TestGoLogger_Warn(t *testing.T) {
+	logger := NewGoLogger(Config{LogLevel: 3})
+	logger.SetScope("TestGoLogger_Warn")
+
+	s := make([]map[string]interface{}, 0)
+
+	s = append(s, H{"s": false})
+	logger.Warn(s)
+	logger.Warn("testing info log")
+	logger.Warn(1)
+}
+
+func TestGoLogger_Warn2(t *testing.T) {
+	logger := NewGoLogger(Config{LogLevel: 3}).SetScope("TestGoLogger_Warn2")
+
+	s := make([]map[string]interface{}, 0)
+
+	s = append(s, H{"s": false})
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		logger.Warn(s)
+		wg.Done()
+	}()
+
+	go func() {
+		logger.Warn(1)
+		wg.Done()
+	}()
+
+	go func() {
+		logger.Warn("testing info log")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func TestGoLogger_Error(t *testing.T) {
+	logger := NewGoLogger(Config{LogLevel: 3})
+	logger.SetScope("TestGoLogger_Error")
+
+	s := make([]map[string]interface{}, 0)
+
+	s = append(s, H{"s": false})
+	logger.Error(s)
+	logger.Error("testing info log")
+	logger.Error(1)
+}
+
+func TestGoLogger_Error2(t *testing.T) {
+	logger := NewGoLogger(Config{LogLevel: 3}).SetScope("TestGoLogger_Warn2")
+
+	s := make([]map[string]interface{}, 0)
+
+	s = append(s, H{"s": false})
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		logger.Error(s)
+		wg.Done()
+	}()
+
+	go func() {
+		logger.Error(1)
+		wg.Done()
+	}()
+
+	go func() {
+		logger.Error("testing info log")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func BenchmarkGoLogger_Info(b *testing.B) {
+	logger := NewGoLogger(Config{LogLevel: 1}).SetScope("TestGoLogger_Info")
+	s := make([]map[string]interface{}, 0)
+
+	s = append(s, H{"s": false})
+	for i:=0; i< b.N; i++ {
+		logger.Info(s)
+		logger.Info("testing info log")
+		logger.Info(1)
+	}
+}
